dfb95794dbd984e0577d19f97ee1a639c2ea466f
Vladyslav Verhun <vv57707@zut.edu.pl>
2026-06-24 05:39:29 +0200

    Complete lab I and J corrections

--- lab-j/hello.py
+++ lab-j/hello.py
@@ -1,22 +1,10 @@
 import sys
-from flask import Flask
 
-app = Flask(__name__)
+name = "Vladyslav"
+album = "57707"
 
-
-@app.route("/")
-def hello():
-    name = "Vladyslav"
-    album = 57707
-    python_version = sys.version.split()[0]
-    python_path = sys.executable
-
-    return (
-        f"Hello {name} ({album}). "
-        f"This environment is using Python version {python_version} "
-        f"at location {python_path}"
-    )
-
-
-if __name__ == "__main__":
-    app.run(port=57707, debug=True)
+print(
+    f"Hello {name} ({album}). "
+    f"This environment is using Python version {sys.version.split()[0]} "
+    f"at location {sys.executable}."
+)
